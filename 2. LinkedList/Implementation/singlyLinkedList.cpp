e06c208dada5dab5e3ec955c4de26ce091753293
Shubham Kumar <shubhamkumarlhh@gmail.com>
2022-02-07 20:49:49 +0530

    Update singlyLinkedList.cpp

--- 2. LinkedList/Implementation/singlyLinkedList.cpp
+++ 2. LinkedList/Implementation/singlyLinkedList.cpp
@@ -1,5 +1,5 @@
 /*
-    SINGLY LINKED LIST 
+    SINGLY LINKED LIST
 
     Operations performed:
     - Check if SLL is empty
@@ -14,52 +14,51 @@
     - Delete end node
 */
 
-
 #include <iostream>
 using namespace std;
 
 template <class T>
-class SinglyLinkedList 
+class SinglyLinkedList
 {
-    private:
-        // Node of the linked list 
-        class Node
-        {
-            public:
-                T data;         // data of the current node
-                Node *next;     // pointer to the next node
-
-                Node(T val) : data(val), next(nullptr) {}
-                Node() : data{}, next(nullptr) {}
-        };
-
-        // points to start of the linked list
-        Node *head;
-
-        // creates new node 
-        Node* newNode(T);
-
+private:
+    // Node of the linked list
+    class Node
+    {
     public:
-        SinglyLinkedList() : head(nullptr) {}
-        ~SinglyLinkedList();
-        bool isEmpty();             // Returns true if list is empty, otherwise false
-        void display();             // Traverses and prints the linked list
-        void insertBeg(T);          // Inserts a node at the beginning of the linked list
-        void insertEnd(T);          // Inserts a node at the end of the linked list
-        void insertBefore(T, T);    // Inserts a node before the node with given data
-        void insertAfter(T, T);     // Inserts a node after the node with given data
-        bool search(T);             // Returns true if a node with given data is found  
-        int totalNodes();           // Returns total number of node in the linked list 
-        void deleteBeg();           // Delete a node from the beginning of the linked list 
-        void deleteEnd();           // Delete a node from the end of the linked list 
+        T data;     // data of the current node
+        Node *next; // pointer to the next node
+
+        Node(T val) : data(val), next(nullptr) {}
+        Node() : data{}, next(nullptr) {}
+    };
+
+    // points to start of the linked list
+    Node *head;
+
+    // creates new node
+    Node *newNode(T);
+
+public:
+    SinglyLinkedList() : head(nullptr) {}
+    ~SinglyLinkedList();
+    bool isEmpty();          // Returns true if list is empty, otherwise false
+    void display();          // Traverses and prints the linked list
+    void insertBeg(T);       // Inserts a node at the beginning of the linked list
+    void insertEnd(T);       // Inserts a node at the end of the linked list
+    void insertBefore(T, T); // Inserts a node before the node with given data
+    void insertAfter(T, T);  // Inserts a node after the node with given data
+    bool search(T);          // Returns true if a node with given data is found
+    int totalNodes();        // Returns total number of node in the linked list
+    void deleteBeg();        // Delete a node from the beginning of the linked list
+    void deleteEnd();        // Delete a node from the end of the linked list
 };
 
 // creates and returns new node of the linked list
-template<class T>
-typename SinglyLinkedList<T>::Node* SinglyLinkedList<T>::newNode(T val)
+template <class T>
+typename SinglyLinkedList<T>::Node *SinglyLinkedList<T>::newNode(T val)
 {
-    Node* ptr = new Node(val);
-    
+    Node *ptr = new Node(val);
+
     if (ptr == nullptr)
     {
         cout << "Overflow!!!" << endl;
@@ -70,17 +69,17 @@ typename SinglyLinkedList<T>::Node* SinglyLinkedList<T>::newNode(T val)
 }
 
 // Returns true if linked list is empty - O(1)
-template<class T>
-bool SinglyLinkedList<T>::isEmpty() 
+template <class T>
+bool SinglyLinkedList<T>::isEmpty()
 {
     if (head == nullptr)
         return true;
-    else 
+    else
         return false;
 }
 
 // Traverse and print the linked list - O(n)
-template<class T>
+template <class T>
 void SinglyLinkedList<T>::display()
 {
     if (this->isEmpty())
@@ -89,9 +88,9 @@ void SinglyLinkedList<T>::display()
         return;
     }
 
-    Node* ptr = head;
-    
-    while(ptr != nullptr) 
+    Node *ptr = head;
+
+    while (ptr != nullptr)
     {
         cout << ptr->data << " ";
         ptr = ptr->next;
@@ -100,36 +99,36 @@ void SinglyLinkedList<T>::display()
 }
 
 // Inserts the node at the beginning of the linked list - O(1)
-template<class T>
+template <class T>
 void SinglyLinkedList<T>::insertBeg(T val)
 {
-    Node* ptr = newNode(val);
+    Node *ptr = newNode(val);
     ptr->next = head;
     head = ptr;
 }
 
 // Inserts the node at the end of the linked list - O(n)
-template<class T>
-void SinglyLinkedList<T>::insertEnd(T val) 
+template <class T>
+void SinglyLinkedList<T>::insertEnd(T val)
 {
-    Node* ptr = newNode(val);
-    Node* curr = head;
+    Node *ptr = newNode(val);
+    Node *curr = head;
 
-    if (this->isEmpty) 
+    if (this->isEmpty)
     {
         head = ptr;
         return;
     }
 
-    while(curr->next != nullptr)
+    while (curr->next != nullptr)
         curr = curr->next;
-    
+
     curr->next = ptr;
 }
 
 // Inserts the node before the node with the given data - O(n)
-template<class T>
-void SinglyLinkedList<T>::insertBefore(T val, T data) 
+template <class T>
+void SinglyLinkedList<T>::insertBefore(T val, T data)
 {
     if (this->isEmpty() || head->data == data)
     {
@@ -137,17 +136,17 @@ void SinglyLinkedList<T>::insertBefore(T val, T data)
         return;
     }
 
-    Node* ptr = newNode(val);
-    Node* curr = head;
-    Node* prev = nullptr;
-    
-    while (curr != nullptr && curr->data != data) 
+    Node *ptr = newNode(val);
+    Node *curr = head;
+    Node *prev = nullptr;
+
+    while (curr != nullptr && curr->data != data)
     {
         prev = curr;
         curr = curr->next;
     }
 
-    if (curr == nullptr) 
+    if (curr == nullptr)
     {
         cout << "No node present with data equals " << data << "!!!" << endl;
         return;
@@ -158,22 +157,22 @@ void SinglyLinkedList<T>::insertBefore(T val, T data)
 }
 
 // Inserts the node after the node with given data - O(n)
-template<class T>
-void SinglyLinkedList<T>::insertAfter(T val, T data) 
+template <class T>
+void SinglyLinkedList<T>::insertAfter(T val, T data)
 {
-    if (this->isEmpty() || head->data == data) 
+    if (this->isEmpty() || head->data == data)
     {
         this->insertEnd(val);
         return;
     }
 
-    Node* ptr = newNode(val);
-    Node* curr = head;
+    Node *ptr = newNode(val);
+    Node *curr = head;
 
-    while (curr != nullptr && curr->data != data) 
+    while (curr != nullptr && curr->data != data)
         curr = curr->next;
 
-    if (curr == nullptr) 
+    if (curr == nullptr)
     {
         cout << "No node present with data equals " << data << "!!!" << endl;
         return;
@@ -184,19 +183,19 @@ void SinglyLinkedList<T>::insertAfter(T val, T data)
 }
 
 // Returns true if a node with data equals val is found - O(n)
-template<class T>
-bool SinglyLinkedList<T>::search(T val) 
+template <class T>
+bool SinglyLinkedList<T>::search(T val)
 {
-    if (this->isEmpty()) 
+    if (this->isEmpty())
         return false;
 
-    Node* curr = head;
+    Node *curr = head;
 
-    while(curr != nullptr) 
+    while (curr != nullptr)
     {
         if (val == curr->data)
             return true;
-        else 
+        else
             curr = curr->next;
     }
 
@@ -204,61 +203,61 @@ bool SinglyLinkedList<T>::search(T val)
 }
 
 // Returns the count of total number of nodes present - O(n)
-template<class T>
-int SinglyLinkedList<T>::totalNodes() 
+template <class T>
+int SinglyLinkedList<T>::totalNodes()
 {
     int count = 0;
-    Node* curr = head;
-    
-    while (curr != nullptr) 
+    Node *curr = head;
+
+    while (curr != nullptr)
     {
         count++;
         curr = curr->next;
     }
-    
+
     return count;
 }
 
 // Deletes a node from the beginning of the linked list - O(1)
 template <class T>
-void SinglyLinkedList<T>::deleteBeg() 
+void SinglyLinkedList<T>::deleteBeg()
 {
-    if (this->isEmpty()) 
+    if (this->isEmpty())
     {
         cout << "Underflow!!!" << endl;
         return;
     }
 
-    Node* temp = head;
+    Node *temp = head;
     head = head->next;
-    
+
     delete temp;
 }
 
-// Deletes a node from the end of the linked list - O(n) 
+// Deletes a node from the end of the linked list - O(n)
 template <class T>
-void SinglyLinkedList<T>::deleteEnd() 
+void SinglyLinkedList<T>::deleteEnd()
 {
-    if (this->isEmpty()) 
+    if (this->isEmpty())
     {
         cout << "Underflow!!!" << endl;
         return;
     }
 
-    if (head->next == nullptr) 
+    if (head->next == nullptr)
     {
-        Node* temp = head;
+        Node *temp = head;
         head = nullptr;
         delete temp;
         return;
     }
 
-    Node* curr = head;
-    Node* prev = nullptr;
+    Node *curr = head;
+    Node *prev = nullptr;
 
-    while (curr->next != nullptr) 
+    while (curr->next != nullptr)
     {
-        prev = curr; 
+        prev = curr;
         curr = curr->next;
     }
 
