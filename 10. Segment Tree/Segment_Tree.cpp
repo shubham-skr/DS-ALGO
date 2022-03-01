49748ec75bc919f11705f03ba70b39e59b05ceee
Shubham Kumar <shubhamkumarlhh@gmail.com>
2022-03-01 21:00:12 +0530

    Update Segment_Tree.cpp

--- 10. Segment Tree/Segment_Tree.cpp
+++ 10. Segment Tree/Segment_Tree.cpp
@@ -1,10 +1,9 @@
 /*
 
-SEGMENT TREE
+SEGMENT TREE (with lazy propagation)
     - Build, Query and Update
 
 */
-
 
 #include <vector>
 using namespace std;
@@ -15,19 +14,20 @@ class SegmentTree
 private:
     // size of the segment tree
     int n;
-    // segment tree
-    vector<int> st;
+    // segment tree and lazy tree
+    vector<int> st, lazy;
 
     // overloaded functions
     void build(vector<int> &, int, int, int);
     int query(int, int, int, int, int);
-    void update(int, int, int, int, int);
+    void update(int, int, int, int, int, int);
 
 public:
     SegmentTree(int n)
     {
         this->n = n;
         st.resize(4 * n, 0);
+        lazy.resize(4 * n, 0);
     }
 
     // Build the segment tree
@@ -43,9 +43,9 @@ public:
     }
 
     // Update value at x to y
-    void update(int x, int y)
+    void update(int l, int r, int x)
     {
-        update(0, n - 1, 0, x, y);
+        update(0, n - 1, 0, l, r, x);
     }
 };
 
@@ -77,8 +77,22 @@ int SegmentTree::query(int start, int end, int l, int r, int node)
         return 0;
     }
 
+    // lazy propagation
+    if (lazy[node] != 0)
+    {
+        // update the segment tree node
+        st[node] = lazy[node] * (end - start + 1);
+        if (start != end)
+        {
+            // propagate tge update value
+            lazy[2 * node + 1] += lazy[node];
+            lazy[2 * node + 2] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
     // complete overlap
-    if (start == l && end == r)
+    if (start >= l && end <= r)
     {
         return st[node];
     }
@@ -92,27 +106,47 @@ int SegmentTree::query(int start, int end, int l, int r, int node)
     return q1 + q2;
 }
 
-void SegmentTree::update(int start, int end, int node, int index, int value)
+void SegmentTree::update(int start, int end, int node, int l, int r, int value)
 {
-    // base case
-    if (start == end)
+    // non-overlapping case
+    if (start > r || end < l)
     {
-        st[node] = value;
         return;
     }
 
+    // lazy propagation
+    // clear the lazy update
+    if (lazy[node] != 0)
+    {
+        // update the segment tree node
+        st[node] += lazy[node] * (end - start + 1);
+        if (start != end)
+        {
+            // propagate the updated value
+            lazy[2 * node + 1] += lazy[node];
+            lazy[2 * node + 2] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
+    // complete overlap
+    if (start >= l && end <= r)
+    {
+        st[node] += value * (end - start + 1);
+        if (start != end)
+        {
+            lazy[2 * node + 1] += value;
+            lazy[2 * node + 2] += value;
+        }
+        return;
+    }
+
+    // partial case
     int mid = (start + end) / 2;
 
-    // left subtree
-    if (index <= mid)
-    {
-        update(start, mid, 2 * node + 1, index, value);
-    }
-    // right subtree
-    else
-    {
-        update(mid + 1, end, 2 * node + 2, index, value);
-    }
+    update(start, mid, 2 * node + 1, l, r, value);
+
+    update(mid + 1, end, 2 * node + 2, l, r, value);
 
     st[node] = st[2 * node + 1] + st[2 * node + 2];
 }
