497ab4636f8c900d2ebdefcc23e029c013638e78
Shubham Kumar <shubhamkumarlhh@gmail.com>
2022-03-29 08:37:18 +0530

    initial commit

--- 15. Trie/trie.cpp
+++ 15. Trie/trie.cpp
@@ -1,11 +1,9 @@
 /*
-
-TRIE Data Structure
-
-- efficient information reTrieval data structure
-- searching a key takes O(N) time where N is key length
-
+    
+    TRIE Data Structure
+    
 */
+
 
 #include <string>
 using namespace std;
@@ -35,6 +33,9 @@ class Trie
 private:
 	TrieNode *root;
 
+	bool isEmpty(TrieNode *rootPtr);
+	TrieNode *remove(TrieNode *, string, int);
+
 public:
 	Trie()
 	{
@@ -43,20 +44,30 @@ public:
 
 	void insert(string);
 	bool search(string);
+	void remove(string);
 };
+
+
+bool Trie::isEmpty(TrieNode *rootPtr)
+{
+	for (int i = 0; i < ALPHABET_SIZE; i++)
+		if (rootPtr->children[i])
+			return false;
+	return true;
+}
 
 
 void Trie::insert(string key)
 {
 	TrieNode *ptr = root;
-	
+
 	for (int i = 0; i < key.length(); i++)
 	{
 		int index = key[i] - 'a';
-		
+
 		if (!ptr->children[index])
 			ptr->children[index] = new TrieNode();
-		
+
 		ptr = ptr->children[index];
 	}
 
@@ -67,16 +78,53 @@ void Trie::insert(string key)
 bool Trie::search(string key)
 {
 	TrieNode *ptr = root;
-	
+
 	for (int i = 0; i < key.length(); i++)
 	{
 		int index = key[i] - 'a';
-		
+
 		if (!ptr->children[index])
 			return false;
-		
+
 		ptr = ptr->children[index];
 	}
 
 	return ptr->isEndOfWord;
+}
+
+TrieNode *Trie::remove(TrieNode *rootPtr, string key, int depth = 0)
+{
+	if (!rootPtr)
+		return nullptr;
+
+	if (depth == key.length())
+	{
+		if (rootPtr->isEndOfWord)
+			root->isEndOfWord = false;
+
+		if (isEmpty(rootPtr))
+		{
+			delete rootPtr;
+			rootPtr = nullptr;
+		}
+
+		return rootPtr;
+	}
+
+	int index = key[depth] - 'a';
+	rootPtr->children[index] = remove(rootPtr->children[index], key, depth + 1);
+
+	if (isEmpty(rootPtr) && rootPtr->isEndOfWord == false)
+	{
+		delete (rootPtr);
+		rootPtr = nullptr;
+	}
+
+	return rootPtr;
+}
+
+
+void Trie::remove(string key)
+{
+	root = remove(root, key, 0);
 }
